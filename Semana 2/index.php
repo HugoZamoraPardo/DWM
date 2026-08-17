d2894f3fafc0f4698aaee82c21bd4be994bfa6e7
SofiaRamosZ <sofy.ramos.zuniga1@gmail.com>
2026-08-17 17:11:52 -0400

    Update greeting message in index.php

--- Semana 2/index.php
+++ Semana 2/index.php
@@ -3,6 +3,6 @@
         <title>Primera página</title>
     </head>
     <body>
-        <h1>Heey muy buenas a todos, guaapísimos</h1>
+        <h1>Heey muy buenas a todos, guaapísimos aqui vegetta777</h1>
     </body>
 </html
